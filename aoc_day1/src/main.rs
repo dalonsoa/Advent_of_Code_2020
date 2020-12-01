0078a51e708301e4b797591d12b3a50cd8792324
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-12-01 15:54:46 +0000

    :bug: Solve error happening if target == 2*first_number

--- aoc_day1/src/main.rs
+++ aoc_day1/src/main.rs
@@ -18,11 +18,11 @@ fn get_nums(args: &[String]) -> Vec<i32> {
 /// 
 /// Recursive function that scans a vector and looks for two numbers that 
 /// add up to a third one. If found, return their product. Otherwise, return zero.
-fn sum_two_nums(num: &i32, numbers: &[i32], target: &i32) -> i32 {
+fn sum_two_nums(num1: &i32, numbers: &[i32], target: &i32) -> i32 {
     
     for num2 in numbers.iter() {
-        if num + num2 == *target {
-            return num * num2;
+        if num1 + num2 == *target {
+            return num1 * num2;
         }
     };
 
@@ -37,14 +37,14 @@ fn sum_two_nums(num: &i32, numbers: &[i32], target: &i32) -> i32 {
 /// 
 /// Recursive function that scans a vector and looks for three numbers that 
 /// add up to a third one. If found, return their product. Otherwise, return zero.
-fn sum_three_nums(num: &i32, numbers: &[i32], target: &i32) -> i32 {
+fn sum_three_nums(num1: &i32, numbers: &[i32], target: &i32) -> i32 {
     let mut counter: usize = 1;
 
     for num2 in numbers.iter() {
-        if num + num2 < *target {
+        if num1 + num2 < *target {
             for num3 in numbers[counter..].iter() {
-                if num + num2 + num3 == *target {
-                    return num*num2*num3;
+                if num1 + num2 + num3 == *target {
+                    return num1 * num2 * num3;
                 }
             }
         }
@@ -63,6 +63,6 @@ fn main() {
 
     let numbers: Vec<i32> = get_nums(&args);
 
-    println!("Two numbers: {:?}", sum_two_nums(&numbers[0], &numbers[..], &2020));
-    println!("Three numbers: {:?}", sum_three_nums(&numbers[0], &numbers[..], &2020));
+    println!("Two numbers: {:?}", sum_two_nums(&numbers[0], &numbers[1..], &2020));
+    println!("Three numbers: {:?}", sum_three_nums(&numbers[0], &numbers[1..], &2020));
 }
