77fed4d6bd012dfb5afc001d7a11349b327107c3
Diego <d.alonso-alvarez@imperial.ac.uk>
2020-12-02 14:43:28 +0000

    :bug: Strip trailing spaces

--- aoc_day2/src/main.rs
+++ aoc_day2/src/main.rs
@@ -1,6 +1,6 @@
+use regex::Regex;
 use std::env;
 use std::fs;
-use regex::Regex;
 
 #[derive(Debug)]
 struct Password {
@@ -12,19 +12,20 @@ struct Password {
 
 impl Password {
     /// Parse a string using Regex to get the password parameters
-    /// 
+    ///
     /// The input string must have format "low-high letter: password"
-    /// 
+    ///
     /// It is not fast...
     fn from_str(raw: String) -> Password {
         let re = Regex::new(r"(\d+)\W(\d+)\s(\w)\W+(\w+)").unwrap();
         let data = re.captures(&raw).unwrap();
 
-        Password{
-            low: data.get(1).unwrap().as_str().parse::<usize>().unwrap(), 
-            high: data.get(2).unwrap().as_str().parse::<usize>().unwrap(), 
-            letter: data.get(3).unwrap().as_str().to_string(), 
-            pwd: data.get(4).unwrap().as_str().to_string(),}
+        Password {
+            low: data.get(1).unwrap().as_str().parse::<usize>().unwrap(),
+            high: data.get(2).unwrap().as_str().parse::<usize>().unwrap(),
+            letter: data.get(3).unwrap().as_str().to_string(),
+            pwd: data.get(4).unwrap().as_str().to_string(),
+        }
     }
 
     /// Validates the password according to the old policy
@@ -35,11 +36,10 @@ impl Password {
 
     /// Validates the password according to the new policy
     fn is_valid_new(&self) -> bool {
-        (self.pwd.as_bytes()[self.low-1] == self.letter.as_bytes()[0]) ^ 
-        (self.pwd.as_bytes()[self.high-1] == self.letter.as_bytes()[0])
+        (self.pwd.as_bytes()[self.low - 1] == self.letter.as_bytes()[0])
+            ^ (self.pwd.as_bytes()[self.high - 1] == self.letter.as_bytes()[0])
     }
 }
-
 
 /// Reads a text file with policy and passwords
 fn get_passwords(args: &[String]) -> Vec<Password> {
@@ -49,8 +49,9 @@ fn get_passwords(args: &[String]) -> Vec<Password> {
 
     fs::read_to_string(filename)
         .expect("Something went wrong reading the file")
+        .trim_end()
         .split("\n")
-        .map(|s| Password::from_str(s.parse::<String>().unwrap()))
+        .map(|s| Password::from_str(s.to_string()))
         .collect()
 }
 
@@ -65,13 +66,20 @@ fn main() {
     for pwd in passwords.iter() {
         valid_old += pwd.is_valid_old() as usize
     }
-    println!("Old policy valid passwords: {}/{}", valid_old, passwords.len());
+    println!(
+        "Old policy valid passwords: {}/{}",
+        valid_old,
+        passwords.len()
+    );
 
     // Checking new policy
     println!("Checking new policy...");
     for pwd in passwords.iter() {
         valid_new += pwd.is_valid_new() as usize
     }
-    println!("New policy valid passwords: {}/{}", valid_new, passwords.len());
-
+    println!(
+        "New policy valid passwords: {}/{}",
+        valid_new,
+        passwords.len()
+    );
 }
